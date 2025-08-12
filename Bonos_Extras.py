eef99684123de05798ef7277f3a71c73db8517e9
formtpz <137564298+formtpz@users.noreply.github.com>
2025-08-12 17:12:41 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -546,12 +546,14 @@ def Bonos_Extras(usuario,puesto):
       error_9 = placeholder32_9.error('No existen datos para mostrar')
     else:
       # Reemplaza nulos por 0 y suma solo las columnas necesarias
-      bonos_sum = bonos_9[['a99', 'a100', 'a102']].fillna(0).sum().sum()
-
+      #bonos_sum = bonos_9[['a99', 'a100', 'a102']].fillna(0).sum().sum()
+      bonos_variables_9 = sum([float(bonos_9.iloc[0,99]),float(bonos_9.iloc[0,100]))
+      bonos_fijos_9 = sum([float(bonos_9.iloc[0,99]),float(bonos_9.iloc[0,102]))
+      
       placeholder33_9 = st.empty()
       col1, col2 = placeholder33_9.columns(2)
-      col1.metric("Suma de a99+a100+a102", bonos_sum)
-      col2.metric("Total", bonos_sum)  # Aquí puedes cambiar si quieres otro total
+      col1.metric("Bonos Variables", bonos_variables_9)
+      col2.metric("Bonos Fijos", bonos_fijos_9)  # Aquí puedes cambiar si quieres otro total
 
 
       # Procesos #
