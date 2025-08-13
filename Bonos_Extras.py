1ca6e0fc02786b28ed7ef35a23c2bdb9c664da01
formtpz <137564298+formtpz@users.noreply.github.com>
2025-08-13 15:59:03 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -547,8 +547,8 @@ def Bonos_Extras(usuario,puesto):
     else:
       # Reemplaza nulos por 0 y suma solo las columnas necesarias
 
-      bonos_variables_9 = float(bonos_9.iloc[:,3]) + float(bonos_9.iloc[:,1])
-      bonos_fijos_9 = float(bonos_9.iloc[:,3]) + float(bonos_9.iloc[:,2])
+      bonos_variables_9 = float(bonos_9.iloc[0, 3]) + float(bonos_9.iloc[0, 1])
+      bonos_fijos_9 = float(bonos_9.iloc[0, 3]) + float(bonos_9.iloc[0, 2])
       
       placeholder33_9 = st.empty()
       col1, col2 = placeholder33_9.columns(2)
