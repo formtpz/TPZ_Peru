ebbf383f0038cb75ed15bf1ac5d4f7b1784ff266
formtpz <137564298+formtpz@users.noreply.github.com>
2025-08-26 08:25:05 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -144,6 +144,7 @@ if iniciar_sesion_1:
                 st.session_state.Revision_Campo=False
                 st.session_state.Postcampo=False
                 st.session_state.CC_Postcampo=False
+                st.session_state.Estado_UIT_Hito=False
                                     
                 puesto=pd.read_sql(f"select puesto from usuarios where usuario ='{usuario}'",uri)
                 puesto= puesto.loc[0,'puesto']
