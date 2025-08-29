252184bf5c86cee716863b6c6635f37ec4a2539b
formtpz <137564298+formtpz@users.noreply.github.com>
2025-08-29 13:24:56 -0600

    Update CC_Postcampo.py

--- CC_Postcampo.py
+++ CC_Postcampo.py
@@ -63,7 +63,7 @@ def CC_Postcampo(usuario,puesto):
   tipo_3= placeholder13_3.selectbox("Tipo", options=("Inspección","Primera Reinspección"), key="tipo_3")
 
   placeholder14_3= st.empty()
-  tipo_de_errores_3= placeholder14_3.multiselect("Tipo Errores", options=("Errores topológicos","Ejecución de códigos","Dirección","Digitalización de construcciones","Consecutivos de NPN","Derecho interesado fuente","Georreferenciación","Informacion Catastral","Notas en aplicativo","Creación de F","Método de captura","Validadores","Novedades","Rechazo por espacios vacios","N/A"), key="tipo_de_errores_3")
+  tipo_de_errores_3= placeholder14_3.multiselect("Tipo Errores", options=("Numeración errónea o incompleta","Errores geométricos y/o de forma","Polígonos y/o puntos duplicados","Omisión/Comisión de polígonos","Polígonos no se ajustan a ortofoto","Omisión/Comisión de puertas"), key="tipo_de_errores_3")
     
   placeholder15_3= st.empty()
   aprobados_3= placeholder15_3.number_input("Cantidad de Lotes Aprobados",min_value=0,step=1,key="aprobados_3")
