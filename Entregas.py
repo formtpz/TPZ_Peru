aa2246adbe57943d678be877c4cbe9b8d503de00
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-13 15:05:27 -0600

    Update and rename Revision_Campo.py to Entregas.py

--- Entregas.py
+++ Entregas.py
@@ -43,7 +43,7 @@ def Revision_Campo(usuario,puesto):
   salir_3 = placeholder7_3.button("Salir",key="salir_3")
 
   placeholder8_3 = st.empty()
-  revision_campo_3 = placeholder8_3.title("Revisión de Campo")
+  revision_campo_3 = placeholder8_3.title("Entregas")
 
   default_date_3 = datetime.now(pytz.timezone('America/Guatemala'))
 
@@ -54,19 +54,19 @@ def Revision_Campo(usuario,puesto):
   distrito_3= placeholder10_3.selectbox("Distrito", options=("Chorrillos","San Juan De Miraflores","Villa el Salvador"), key="distrito_3")
 
   placeholder11_3= st.empty()
-  consecutivo_3= placeholder11_3.text_input("Número de Paquete",max_chars=15,key="consecutivo_3")
+  sector_3= placeholder11_3.selectbox("Sector", options=("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120"), key="Sector_3")
 
   placeholder12_3= st.empty()
-  tipo_3= placeholder12_3.selectbox("Tipo", options=("Inspección","Primera Reinspección","Segunda Reinspección"), key="tipo_3")
+  manzana_3= placeholder12_3.selectbox("Manzana", options=("001","002","003","004","005","006","007","008","009","010","011","012","013","014","015","016","017","018","019","020","021","022","023","024","025","026","027","028","029","030","031","032","033","034","035","036","037","038","039","040","041","042","043","044","045","046","047","048","049","050","051","052","053","054","055","056","057","058","058","059","060","061","062","063","064","065","066","067","068","069","070","071","072","073","074","075","076","077","078","079","080","081","082","083","084","085","086","087","088","089","090","091","092","093","094","095","096","097","098","099","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120"), key="manzana_3")
 
   placeholder13_3= st.empty()
-  zona_3= placeholder13_3.selectbox("zona", options=("Rural","Urbano"), key="zona_3")
+  estado_3= placeholder13_3.selectbox("Estado", options=("Finalizado","En conflicto"), key="estado_3")
 
-  placeholder14_3= st.empty()
-  aprobados_3= placeholder14_3.number_input("Cantidad de Predios Aprobados",min_value=0,step=1,key="aprobados_3")
+  #placeholder14_3= st.empty()
+  #aprobados_3= placeholder14_3.number_input("Cantidad de Predios Aprobados",min_value=0,step=1,key="aprobados_3")
 
-  placeholder15_3= st.empty()
-  rechazados_3= placeholder15_3.number_input("Cantidad de Predios Rechazados",min_value=0,step=1,key="rechazados_3")
+  #placeholder15_3= st.empty()
+  #rechazados_3= placeholder15_3.number_input("Cantidad de Predios Rechazados",min_value=0,step=1,key="rechazados_3")
   
   placeholder16_3= st.empty()
   horas_3= placeholder16_3.number_input("Cantidad de Horas Trabajadas en el Proceso",min_value=0.0,key="horas_3")
@@ -89,13 +89,13 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
     st.session_state.Procesos=False
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
 
     perfil=pd.read_sql(f"select perfil from usuarios where usuario ='{usuario}'",uri)
     perfil= perfil.loc[0,'perfil']
@@ -127,12 +127,12 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
     st.session_state.Historial=True
     Historial.Historial(usuario,puesto)   
 
@@ -151,12 +151,12 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
     st.session_state.Capacitacion=True
     Capacitacion.Capacitacion(usuario,puesto)
 
@@ -175,12 +175,12 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
     st.session_state.Otros_Registros=True
     Otros_Registros.Otros_Registros(usuario,puesto)
 
@@ -199,12 +199,12 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
     st.session_state.Bonos_Extras=True
     Bonos_Extras.Bonos_Extras(usuario,puesto)    
 
@@ -223,13 +223,13 @@ def Revision_Campo(usuario,puesto):
     placeholder10_3.empty()
     placeholder11_3.empty()
     placeholder12_3.empty()
-    placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder13_3.empty()
+    #placeholder14_3.empty()
     placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
     st.session_state.Ingreso = False
-    st.session_state.Revision_Campo=False
+    st.session_state.Entregas=False
     st.session_state.Salir=True
     Salir.Salir()
 
@@ -251,6 +251,8 @@ def Revision_Campo(usuario,puesto):
 
     año_3 = fecha_3.isocalendar()[0]
 
+    horas_bi = float(horas_3)
+
     # ----- Almacenar Lote_3 según municipio seleccionado ---- #
     
     #lote_3_municipios = {"Cabuyaro", "Colombia", "San Luis de Cubarral"}
@@ -265,6 +267,6 @@ def Revision_Campo(usuario,puesto):
       # ----- Fin del script ---- #
     #unidad_3=municipio_3
     horas_bi = float(horas_3)
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,unidad_asignacion,tipo,produccion,aprobados,rechazados,horas,uit,hito,lote,estado,area,efes,informales,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores)VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Revisión de Campo','{fecha_3}','{semana_3}','{año_3}','{distrito_3}','{tipo_3}','{produccion_3}','{aprobados_3}','{rechazados_3}','{horas_3}','UIT-0','0','{lote_3}','N/A','0.0','0','0','{consecutivo_3}','0','0','N/A','{zona_3}','N/A','{horas_bi}','0','N/A','0','0','N/A','0')")
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,distrito,tipo,lotes,aprobados,rechazados,horas,manzana,sector,numero_lote,estado,area,unidades_catastrales,edificas,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores) VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Entregas','{fecha_3}','{semana_3}','{año_3}','{distrito_3}','N/A','0','0','0','{horas_3}','{manzana_3}','{sector_3}','0','{estado_3}','0.0','0','0','N/A','0','0','N/A','N/A','N/A','{horas_bi}','0.0','N/A','0','0','N/A','0')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
