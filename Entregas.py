a313007403a130325fa704c62203a1081368738e
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-14 08:44:22 -0600

    Update Entregas.py

--- Entregas.py
+++ Entregas.py
@@ -200,7 +200,7 @@ def Entregas(usuario,puesto):
     placeholder11_3.empty()
     placeholder12_3.empty()
     placeholder13_3.empty()
-    placeholder14_3.empty()
+    #placeholder14_3.empty()
     #placeholder15_3.empty()
     placeholder16_3.empty()
     placeholder17_3.empty()
@@ -271,6 +271,6 @@ def Entregas(usuario,puesto):
       # ----- Fin del script ---- #
     #unidad_3=municipio_3
     horas_bi = float(horas_3)
-    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,distrito,tipo,lotes,aprobados,rechazados,horas,manzana,sector,numero_lote,estado,area,unidades_catastrales,edificas,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores) VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Entregas','{fecha_3}','{semana_3}','{año_3}','{distrito_3}','N/A','0','0','0','{horas_3}','{manzana_3}','{sector_3}','0','{estado_3}','0.0','0','0','N/A','0','0','N/A','N/A','N/A','{horas_bi}','0.0','N/A','0','0','{tipo_de_errores_3}','0')")
+    cursor01.execute(f"INSERT INTO registro (marca,usuario,nombre,puesto,supervisor,proceso,fecha,semana,año,distrito,tipo,lotes,aprobados,rechazados,horas,manzana,sector,numero_lote,estado,area,unidades_catastrales,edificas,paquete,con_fmi,sin_fmi,observaciones,zona,tipo_calidad,horas_bi,area_bi,operador_cc,total_de_errores,errores_por_excepciones,tipo_de_errores,conteo_de_errores) VALUES('{marca_3}','{usuario}','{nombre_3}','{puesto}','{supervisor_3}','Entregas','{fecha_3}','{semana_3}','{año_3}','{distrito_3}','N/A','0','0','0','{horas_3}','{manzana_3}','{sector_3}','0','{estado_3}','0.0','0','0','N/A','0','0','N/A','N/A','N/A','{horas_bi}','0.0','N/A','0','0','{tipo_de_errores_3}','{conteo_3}')")
     con.commit()                                                                                                                                 
     st.success('Reporte enviado correctamente')
