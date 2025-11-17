e35fc7edbb2baf7f8260a33fb96c28d84472f1e4
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-17 13:07:27 -0600

    Update Bonos_Extras.py

--- Bonos_Extras.py
+++ Bonos_Extras.py
@@ -752,7 +752,7 @@ def Bonos_Extras(usuario,puesto):
     placeholder40_9 = st.empty()
     titulo_extras_9 = placeholder40_9.subheader("Horas Extras")
 
-    extras_9= pd.read_sql(f"select marca,usuario,nombre,puesto,supervisor,tipo_reporte,justificacion,fecha,horas,semana,dia,fecha_corte,fecha_bono from extras where nombre='{nombre_9}'and tipo_reporte='Extra' or tipo_reporte='Horas Extra' or tipo_reporte='Horas Extra Apoyo Otros Proyectos' and fecha_bono='{periodo_9}'", con)
+    extras_9= pd.read_sql(f"select marca,usuario,nombre,puesto,supervisor,tipo_reporte,justificacion,fecha,horas,semana,dia,fecha_corte,fecha_bono from extras where tipo_reporte='Extra' or tipo_reporte='Horas Extra' or tipo_reporte='Horas Extra Apoyo Otros Proyectos' and nombre='{nombre_9}' and fecha_bono='{periodo_9}'", con)
     extras_9= pd.DataFrame(data=extras_9)
 
     pivot7= len(extras_9.iloc[:,0])
