66ad876b5502483964fbb3bd11d0deb2dd8af8db
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-18 15:23:17 -0600

    Update Autenticacion.py

--- Autenticacion.py
+++ Autenticacion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import psycopg2
 
 uri=st.secrets.db_credentials.URI
 
@@ -7,3 +8,13 @@ def contraseña(usuario):
 
     contraseña= pd.read_sql(f"select contraseña from usuarios where usuario = '{usuario}' AND estado='Activo'",uri)
     return contraseña
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(
+        host="...",
+        user="...",
+        password="...",
+        database="..."
+    )
+
+con = init_connection()
