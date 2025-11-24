053b3ae1530c010137e8e3926ce27819d08a0cb7
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-24 10:44:53 -0600

    Update Autenticacion.py

--- Autenticacion.py
+++ Autenticacion.py
@@ -1,29 +1,26 @@
 import pandas as pd
 import streamlit as st
 import psycopg2
-from sqlalchemy import create_engine
 from urllib.parse import urlparse
 
-@st.cache_resource
-def get_uri():
-    return st.secrets.db_credentials.URI
+uri=st.secrets.db_credentials.URI
+
+def contraseña(usuario):
+
+    contraseña= pd.read_sql(f"select contraseña from usuarios where usuario = '{usuario}' AND estado='Activo'",uri)
+    return contraseña
+
+
+result = urlparse(uri)
+hostname = result.hostname
+database = result.path[1:]
+username = result.username
+pwd = result.password
+port_id = result.port
 
 @st.cache_resource
 def init_connection():
-    uri = get_uri()
-    result = urlparse(uri)
-    return psycopg2.connect(
-        host=result.hostname,
-        dbname=result.path[1:],
-        user=result.username,
-        password=result.password,
-        port=result.port
-    )
+    return psycopg2.connect(host=hostname,dbname= database,user= username,password=pwd,port= port_id)
 
 con = init_connection()
 
-@st.cache_resource
-def get_engine():
-    return create_engine(get_uri())
-
-sql_engine = get_engine()
