d54955b8a6fa038bbff91aa044e53b193abe50ea
formtpz <137564298+formtpz@users.noreply.github.com>
2025-11-24 10:40:45 -0600

    Update Autenticacion.py

--- Autenticacion.py
+++ Autenticacion.py
@@ -1,29 +1,29 @@
 import pandas as pd
 import streamlit as st
 import psycopg2
-from sqlalchemy import create_engine # <--- NUEVA IMPORTACIÓN
+from sqlalchemy import create_engine
 from urllib.parse import urlparse
 
-uri = st.secrets.db_credentials.URI
-
-# --- MANTENER ESTO PARA LOS INSERTS (Precampo.py, etc.) ---
-result = urlparse(uri)
-hostname = result.hostname
-database = result.path[1:]
-username = result.username
-pwd = result.password
-port_id = result.port
+@st.cache_resource
+def get_uri():
+    return st.secrets.db_credentials.URI
 
 @st.cache_resource
 def init_connection():
-    return psycopg2.connect(host=hostname, dbname=database, user=username, password=pwd, port=port_id)
+    uri = get_uri()
+    result = urlparse(uri)
+    return psycopg2.connect(
+        host=result.hostname,
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        port=result.port
+    )
 
 con = init_connection()
 
-# --- AGREGAR ESTO PARA PANDAS (Historial.py, Ingreso.py) ---
 @st.cache_resource
 def get_engine():
-    # SQLAlchemy maneja la reconexión automáticamente
-    return create_engine(uri)
+    return create_engine(get_uri())
 
-sql_engine = get_engine() # <--- Variable que exportaremos
+sql_engine = get_engine()
