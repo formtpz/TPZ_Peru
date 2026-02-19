9a1c4e6cee409dde33689d34ab77ba0b30d73b97
formtpz <137564298+formtpz@users.noreply.github.com>
2026-02-19 16:36:59 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -539,9 +539,9 @@ def Historial(usuario,puesto):
     # ----- Resumen de Producción ---- #
 
     placeholder43_7 = st.empty()
-    producción_7=placeholder43_7.subheader("Resumen de Producción")  
+    producción_7=placeholder43_7.subheader(" ")  
 
-    data_2_r = data_1_r.groupby(["nombre", "fecha"], as_index=False)[["lotes","edificas","horas"]].agg(np.sum)
+    #data_2_r = data_1_r.groupby(["nombre", "fecha"], as_index=False)[["lotes","edificas","horas"]].agg(np.sum)
 
     data_4_r = data_1_r.groupby(["nombre", "semana","proceso"], as_index=False)[["edificas","unidades_catastrales","horas"]].agg(np.sum)
 
@@ -552,11 +552,11 @@ def Historial(usuario,puesto):
 
     else:
 
-      data_2_r ["rendimiento"] = data_2_r["edificas"]/data_2_r["horas"]
-      data_2_r['rendimiento'] *= 8.5 
+      #data_2_r ["rendimiento"] = data_2_r["edificas"]/data_2_r["horas"]
+      #data_2_r['rendimiento'] *= 8.5 
        
       placeholder45_7 = st.empty()
-      historial_7_producción= placeholder45_7.dataframe(data=data_2_r)
+      #historial_7_producción= placeholder45_7.dataframe(data=data_2_r)
 
       placeholder46_7 = st.empty()
       producción_7=placeholder46_7.subheader("Resumen de Producción por Proceso")
