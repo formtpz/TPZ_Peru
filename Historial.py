74a3b82567862648b97cd2b82f602716686429ad
formtpz <137564298+formtpz@users.noreply.github.com>
2026-03-24 16:40:57 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -141,7 +141,8 @@ def Historial(usuario,puesto):
     data_8_r = base_r[
         ~base_r["tipo"].isin([
             "Producción Horas Extras",
-            "Inspección Horas Extras"
+            "Inspección Horas Extras",
+            "Reproceso Horas Extras"
         ])
     ].copy()
     
@@ -149,7 +150,8 @@ def Historial(usuario,puesto):
     data_6_r = base_r[
         base_r["tipo"].isin([
             "Producción Horas Extras",
-            "Inspección Horas Extras"
+            "Inspección Horas Extras",
+            "Reproceso Horas Extras"
         ])
     ].copy()
     
