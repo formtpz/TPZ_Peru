c4cbc670ab32e431b3b965c2cd2ca199d0bf8dee
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-19 10:31:00 -0600

    Update CC_Vinculacion_Precampo.py

--- CC_Vinculacion_Precampo.py
+++ CC_Vinculacion_Precampo.py
@@ -110,7 +110,8 @@ def CC_Vinculacion_Precampo(usuario,puesto):
 "Monserrath Romero Bermudez",
 "Older Torres Blanco",
 "Sebastian Alvarado Camacho",
-"Valeria Centeno Mora"), key="operador_3")
+"Valeria Centeno Mora",
+"Maria Jose Farrier Briceno"), key="operador_3")
 
   placeholder15_3= st.empty()
   tipo_3= placeholder15_3.selectbox("Tipo", options=("Inspección","Primera Reinspección","Inspección Horas Extras","Control de Calidad Supervisión"), key="tipo_3")
