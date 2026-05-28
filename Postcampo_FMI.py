03e948d59bffa71da1b08f93872c3df5256e9574
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-28 11:40:56 -0600

    Update Postcampo_FMI.py

--- Postcampo_FMI.py
+++ Postcampo_FMI.py
@@ -81,7 +81,7 @@ def Postcampo_FMI(usuario,puesto):
     st.session_state.Procesos=False
     st.session_state.Postcampo_FMI=False
 
-    perfil=pd.read_sql(f"select perfil from usuarios where usuario ='{usuario}'",uri)
+    perfil=pd.read_sql(f"select perfil from public.usuarios where usuario ='{usuario}'",uri)
     perfil= perfil.loc[0,'perfil']
 
     if perfil=="1":        
@@ -209,10 +209,10 @@ def Postcampo_FMI(usuario,puesto):
 
     marca_3= datetime.now(pytz.timezone('America/Bogota')).strftime("%Y-%m-%d %H:%M:%S")
     
-    nombre_3= pd.read_sql(f"select nombre from usuarios where usuario ='{usuario}'",uri)
+    nombre_3= pd.read_sql(f"select nombre from public.usuarios where usuario ='{usuario}'",uri)
     nombre_3 = nombre_3.loc[0,'nombre']
       
-    supervisor_3= pd.read_sql(f"select supervisor from usuarios where usuario ='{usuario}'",uri)
+    supervisor_3= pd.read_sql(f"select supervisor from public.usuarios where usuario ='{usuario}'",uri)
     supervisor_3 = supervisor_3.loc[0,'supervisor']
     
     semana_3 = fecha_3.isocalendar()[1]
