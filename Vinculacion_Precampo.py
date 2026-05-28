57e3dce3df17da15c146626b44a8d757a10afa2f
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-28 11:43:16 -0600

    Update Vinculacion_Precampo.py

--- Vinculacion_Precampo.py
+++ Vinculacion_Precampo.py
@@ -190,7 +190,7 @@ def Vinculacion_Precampo(usuario, puesto):
         st.session_state.Vinculacion_Precampo = False
     
         perfil = pd.read_sql(
-            f"select perfil from usuarios where usuario ='{usuario}'",
+            f"select perfil from public.usuarios where usuario ='{usuario}'",
             uri
         ).loc[0, 'perfil']
     
@@ -357,9 +357,9 @@ def Vinculacion_Precampo(usuario, puesto):
         cursor = con.cursor()
 
         marca_3= datetime.now(pytz.timezone('America/Guatemala')).strftime("%Y-%m-%d %H:%M:%S")
-        nombre_3= pd.read_sql(f"select nombre from usuarios where usuario ='{usuario}'",uri)
+        nombre_3= pd.read_sql(f"select nombre from public.usuarios where usuario ='{usuario}'",uri)
         nombre_3 = nombre_3.loc[0,'nombre']
-        supervisor_3= pd.read_sql(f"select supervisor from usuarios where usuario ='{usuario}'",uri)
+        supervisor_3= pd.read_sql(f"select supervisor from public.usuarios where usuario ='{usuario}'",uri)
         supervisor_3 = supervisor_3.loc[0,'supervisor']
         semana_3 = fecha_3.isocalendar()[1]
         año_3 = fecha_3.isocalendar()[0]
