3c5d90822108e95f9a24bbe41579b67b5a77b5dc
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-28 11:43:01 -0600

    Update Revision_Segregados.py

--- Revision_Segregados.py
+++ Revision_Segregados.py
@@ -72,7 +72,7 @@ def Revision_Segregados(usuario,puesto):
     st.session_state.Procesos=False
     st.session_state.Revision_Segregados=False
 
-    perfil=pd.read_sql(f"select perfil from usuarios where usuario ='{usuario}'",uri)
+    perfil=pd.read_sql(f"select perfil from public.usuarios where usuario ='{usuario}'",uri)
     perfil= perfil.loc[0,'perfil']
 
     if perfil=="1":        
@@ -190,10 +190,10 @@ def Revision_Segregados(usuario,puesto):
 
     marca_3= datetime.now(pytz.timezone('America/Bogota')).strftime("%Y-%m-%d %H:%M:%S")
     
-    nombre_3= pd.read_sql(f"select nombre from usuarios where usuario ='{usuario}'",uri)
+    nombre_3= pd.read_sql(f"select nombre from public.usuarios where usuario ='{usuario}'",uri)
     nombre_3 = nombre_3.loc[0,'nombre']
       
-    supervisor_3= pd.read_sql(f"select supervisor from usuarios where usuario ='{usuario}'",uri)
+    supervisor_3= pd.read_sql(f"select supervisor from public.usuarios where usuario ='{usuario}'",uri)
     supervisor_3 = supervisor_3.loc[0,'supervisor']
 
     semana_3 = fecha_3.isocalendar()[1]
