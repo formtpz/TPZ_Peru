2a36f5562894dd3a83988db9e30c90a719348400
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-28 11:42:19 -0600

    Update Preparacion_Insumos.py

--- Preparacion_Insumos.py
+++ Preparacion_Insumos.py
@@ -76,7 +76,7 @@ def Preparacion_Insumos(usuario,puesto):
     st.session_state.Procesos=False
     st.session_state.Preparacion_Insumos=False
 
-    perfil=pd.read_sql(f"select perfil from usuarios where usuario ='{usuario}'",uri)
+    perfil=pd.read_sql(f"select perfil from public.usuarios where usuario ='{usuario}'",uri)
     perfil= perfil.loc[0,'perfil']
 
     if perfil=="1":        
@@ -198,10 +198,10 @@ def Preparacion_Insumos(usuario,puesto):
 
     marca_3= datetime.now(pytz.timezone('America/Guatemala')).strftime("%Y-%m-%d %H:%M:%S")
     
-    nombre_3= pd.read_sql(f"select nombre from usuarios where usuario ='{usuario}'",uri)
+    nombre_3= pd.read_sql(f"select nombre from public.usuarios where usuario ='{usuario}'",uri)
     nombre_3 = nombre_3.loc[0,'nombre']
       
-    supervisor_3= pd.read_sql(f"select supervisor from usuarios where usuario ='{usuario}'",uri)
+    supervisor_3= pd.read_sql(f"select supervisor from public.usuarios where usuario ='{usuario}'",uri)
     supervisor_3 = supervisor_3.loc[0,'supervisor']
  
     semana_3 = fecha_3.isocalendar()[1]
